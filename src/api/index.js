5deada3e7c3f417c24bff381367d00150d0646c9
Ruben <rubenvegajusto@gmail.com>
2023-08-19 16:00:22 +0100

    Redux ToolkitAsyncThunk

--- src/api/index.js
+++ src/api/index.js
@@ -10,5 +10,5 @@ export const getPokemon = () => {
 export const getPokemonDetails = (pokemon) => {
   return axios.get(pokemon.url)
   .then(res => res.data)
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err));
 }
